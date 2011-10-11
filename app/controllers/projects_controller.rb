de29f5d76007cbd1e24e9aeac1cee5f5ca0f5fee
Rui Barbosa <rpbarbosa@gmail.com>
2011-10-12 00:04:22 +0100

    Implemented creating tickets for a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  
+
   before_filter :find_project, :only => [:show,
                                          :edit,
                                          :update,
