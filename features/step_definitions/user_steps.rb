7eaf231c208b099bc212b7602564a21e2eaef469
Rui Barbosa <rpbarbosa@gmail.com>
2011-10-13 17:34:56 +0100

    Added feature for signing in via the Devise-provided form

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,6 +2,8 @@ Given /^there are the following users:$/ do |table|
   # table is a Cucumber::Ast::Table
   #pending # express the regexp above with the code you wish you had
   table.hashes.each do |attributes|
+    unconfirmed = attributes.delete("unconfirmed") == "true"
     @user = User.create!(attributes)
+    @user.confirm! unless unconfirmed
   end
 end
